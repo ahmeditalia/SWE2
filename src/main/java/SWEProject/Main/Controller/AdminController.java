36885a78dc279a284ba9d3a5c59bdab82fab3b65
MOhamedHDrmi <hdrmim7@gmail.com>
2018-03-15 23:20:33 +0200

    new pages and update

--- src/main/java/SWEProject/Main/Controller/AdminController.java
+++ src/main/java/SWEProject/Main/Controller/AdminController.java
@@ -1,15 +1,40 @@
 package SWEProject.Main.Controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import SWEProject.Main.Controller.Entities.Store;
+import SWEProject.Main.Controller.Entities.StoreOwner;
+
+import java.util.ArrayList;
+import java.util.List;
 
 
 @Controller
 public class AdminController {
 	
-	@GetMapping("/admin-view")
-	public String loadView()
+
+	@GetMapping("/view-request-stores")
+	public String Viewrequests(Model model)
 	{
+		List<Store> stores = new ArrayList<Store>();
+		Store store;
+		StoreOwner storeOwner;
+		for(int i=1;i<10;i++) {
+			storeOwner=new StoreOwner("A"+i, "B"+i, "asdfasdf"+i);
+			store=new Store("S"+i,storeOwner) {
+			}; 
+			stores.add(store);
+		}
+		model.addAttribute("stores",stores);
+		return "view-request-stores";
+	}
+	
+	
+	@GetMapping("/admin-view")
+	public String loadView(Model model)
+	{
+		model.addAttribute("N",0);
 		return "admin-view";
 	}
 	
--- src/main/java/SWEProject/Main/Controller/ProductController.java
+++ src/main/java/SWEProject/Main/Controller/ProductController.java
@@ -1,4 +1,5 @@
 package SWEProject.Main.Controller;
+
 import SWEProject.Main.Controller.Entities.Brand;
 import SWEProject.Main.Controller.Entities.Product;
 import SWEProject.Main.Controller.Entities.SystemProduct;
@@ -18,45 +19,38 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class ProductController {
-    @Autowired
-    private SystemProductRepository sysProductrepo;
-    @Autowired
-    private StoreProductRepository storeProductRepo;
-    @Autowired
-    private BrandRepository brandRepository;
-    
-    
-    @GetMapping("/add-product-to-system")
-    public String showProductForm(Model model){
-        model.addAttribute("product",new SystemProduct());
-        return "add-product-to-system";
-    }
-    
-    
-    @PostMapping("/add-product-to-system")
-    public String addProduct(@ModelAttribute SystemProduct product){
-        Brand brand = new Brand();
-        brand.setName(product.getBrand().getName());
-        brand.setCategory(product.getBrand().getCategory());
+	@Autowired
+	private SystemProductRepository sysProductrepo;
+	@Autowired
+	private StoreProductRepository storeProductRepo;
+	@Autowired
+	private BrandRepository brandRepository;
 
-        System.out.println(brand.getCategory() + " " + brand.getName());
-        brandRepository.save(brand);
-        product.setBrand(brand);
-        sysProductrepo.save(product);
-        return "redirect:/show-all-product";
-    }
-    
-    
-    @RequestMapping("/show-all-product")
-    public String showAllProducts(Model model)
-    {
-    	Iterable<SystemProduct> pro=sysProductrepo.findAll();
-    	List<Product> products=new ArrayList<Product>();
-    	for(Product p:pro)
-    	{
-    		products.add(p);
-    	}
-    	model.addAttribute("products",products);
-    	return "show-all-product";
-    }
+	@GetMapping("/add-product-to-system")
+	public String showProductForm(Model model) {
+		model.addAttribute("product", new SystemProduct());
+		return "add-product-to-system";
+	}
+
+	@PostMapping("/add-product-to-system")
+	public String addProduct(@ModelAttribute SystemProduct product) {
+		Brand brand = new Brand();
+		brand.setName(product.getBrand().getName());
+		brand.setCategory(product.getBrand().getCategory());
+		brandRepository.save(brand);
+		product.setBrand(brand);
+		sysProductrepo.save(product);
+		return "redirect:/show-all-product";
+	}
+
+	@RequestMapping("/show-all-product")
+	public String showAllProducts(Model model) {
+		Iterable<SystemProduct> pro = sysProductrepo.findAll();
+		List<Product> products = new ArrayList<Product>();
+		for (Product p : pro) {
+			products.add(p);
+		}
+		model.addAttribute("products", products);
+		return "show-all-product";
+	}
 }
