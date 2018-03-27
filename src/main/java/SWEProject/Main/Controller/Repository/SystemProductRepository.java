5ea4870c9bcc786eff53bf21c59ca797710b1118
ahmeditalia <34543951+ahmeditalia@users.noreply.github.com>
2018-03-28 00:19:26 +0200

    fixing program

--- src/main/java/SWEProject/Main/Controller/Repository/SystemProductRepository.java
+++ src/main/java/SWEProject/Main/Controller/Repository/SystemProductRepository.java
@@ -1,15 +1,13 @@
 package SWEProject.Main.Controller.Repository;
 
+import java.util.List;
+
 import org.springframework.data.repository.CrudRepository;
 
 import SWEProject.Main.Controller.Entities.SystemProduct;
 
 public interface SystemProductRepository extends CrudRepository<SystemProduct, Integer>{
 
-<<<<<<< HEAD
     List<SystemProduct> findByBrand(String name);
     boolean existsByName(String name);
-
-=======
->>>>>>> parent of 4258e5f... fix add product
 }
