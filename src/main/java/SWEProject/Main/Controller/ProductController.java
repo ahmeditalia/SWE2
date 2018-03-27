e0d30c52184d0059a873d2ba0c7f14ee7c5987eb
MOhamedHDrmi <hdrmim7@gmail.com>
2018-03-27 20:27:07 +0200

    front end

--- src/main/java/SWEProject/Main/Controller/ProductController.java
+++ src/main/java/SWEProject/Main/Controller/ProductController.java
@@ -2,6 +2,9 @@ package SWEProject.Main.Controller;
 
 import SWEProject.Main.Controller.Entities.Brand;
 import SWEProject.Main.Controller.Entities.Product;
+import SWEProject.Main.Controller.Entities.Store;
+import SWEProject.Main.Controller.Entities.StoreOwner;
+import SWEProject.Main.Controller.Entities.StoreProduct;
 import SWEProject.Main.Controller.Entities.SystemProduct;
 import SWEProject.Main.Controller.Repository.BrandRepository;
 import SWEProject.Main.Controller.Repository.StoreProductRepository;
@@ -10,12 +13,20 @@ import SWEProject.Main.Controller.Repository.SystemProductRepository;
 import java.util.ArrayList;
 import java.util.List;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.util.MimeTypeUtils;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+import com.mysql.fabric.Response;
 
 @Controller
 public class ProductController {
@@ -41,6 +52,24 @@ public class ProductController {
 		product.setBrand(brand);
 		sysProductrepo.save(product);
 		return "redirect:/show-all-product";
+	}
+
+	/*test fnction*/
+	@RequestMapping("/products")
+	public @ResponseBody List<Product> products() {
+		StoreProduct storeProduct;
+		List<Product> products = new ArrayList<Product>();
+		for (int i = 0; i < 10; i++) {
+			storeProduct = new StoreProduct();
+			storeProduct.setBrand(new Brand("" + i, "" + i * 2));	
+			storeProduct.setId(i);
+			storeProduct.setName("" + i);
+			storeProduct.setQuantity(i * i);
+			storeProduct.setType(" Store Type ");
+			storeProduct.setStore(new Store("S" + i, new StoreOwner("a", "b", "c"), "location", "type", "state"));
+			products.add(storeProduct);
+		}
+		return products;
 	}
 
 	@RequestMapping("/show-all-product")
