0ed1fbdf9e454ad03ee29b1f92519deeeb30d81b
hosameldeenreda <34553872+hosameldeenreda@users.noreply.github.com>
2018-03-27 23:29:48 +0200

    addproducttostore

--- src/main/java/SWEProject/Main/Controller/Repository/BrandRepository.java
+++ src/main/java/SWEProject/Main/Controller/Repository/BrandRepository.java
@@ -1,8 +1,11 @@
 package SWEProject.Main.Controller.Repository;
 
 import SWEProject.Main.Controller.Entities.Brand;
+import SWEProject.Main.Controller.Entities.SystemProduct;
+import com.sun.tools.javac.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 public interface BrandRepository  extends CrudRepository<Brand,Integer> {
+    List<Brand> findByProduct(String name);
 
 }
--- src/main/java/SWEProject/Main/Controller/Repository/SystemProductRepository.java
+++ src/main/java/SWEProject/Main/Controller/Repository/SystemProductRepository.java
@@ -1,9 +1,10 @@
 package SWEProject.Main.Controller.Repository;
 
+import com.sun.tools.javac.util.List;
 import org.springframework.data.repository.CrudRepository;
 
 import SWEProject.Main.Controller.Entities.SystemProduct;
 
 public interface SystemProductRepository extends CrudRepository<SystemProduct, Integer>{
-
+    List<SystemProduct> findByBrand(String name);
 }
