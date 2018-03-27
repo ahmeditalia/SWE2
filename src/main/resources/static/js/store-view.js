a591bc9949130c2039e165068c40e76aa2a1f651
MOhamedHDrmi <hdrmim7@gmail.com>
2018-03-28 01:07:49 +0200

    store-view js update

--- src/main/resources/static/js/store-view.js
+++ src/main/resources/static/js/store-view.js
@@ -8,22 +8,23 @@ $(document).ready(function() {
 			$("div#addproduct").hide(500);
 			$("div#table").show(900);
 		});
-	$('#list').change(function() {
-		$.getJSON("/products",function(data){
-			$("#products").empty();
-			for(var i in data)
-			{
-				var tblRow = "<tr>" 
-			+ "<td>" + data[i].id + "</td>" 
-			+ "<td>" + data[i].name + "</td>" 
-           	+ "<td>" + data[i].price + "</td>"
-           	+ "<td>" + data[i].type + "</td>" 
-           	+ "<td>" + data[i].brand + "</td>" 
-           	+ "<td>" + data[i].category + "</td>" 
-           	+ "<td>" + data[i].quantity + "</td>" 
-           	+ "</tr>"
-           	$(tblRow).appendTo("#products tbody");
-			}
-	});
-	});	
+		$('#list').change(function() {
+			$.getJSON("/products",function(data){
+				$("#products tbody").remove();
+				$("#products").append('<tbody></tbody>');
+				for(var i in data)
+				{
+					var tblRow = "<tr>" 
+				+ "<td>" + data[i].id + "</td>" 
+				+ "<td>" + data[i].name + "</td>" 
+	           	+ "<td>" + data[i].price + "</td>"
+	           	+ "<td>" + data[i].type + "</td>" 
+	           	+ "<td>" + data[i].brand + "</td>" 
+	           	+ "<td>" + data[i].category + "</td>" 
+	           	+ "<td>" + data[i].quantity + "</td>" 
+	           	+ "</tr>"
+	           	$("#products tbody").append(tblRow);
+				}
+		});
+		});	
 });
