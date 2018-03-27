31ad47f92c923b6ba61d8bfc6c7863ff6c5c4cfa
MOhamedHDrmi <hdrmim7@gmail.com>
2018-03-28 01:52:37 +0200

    js updates

--- src/main/resources/static/js/add-product-to-system.js
+++ src/main/resources/static/js/add-product-to-system.js
@@ -1,5 +1,7 @@
 $(document).ready(function() {
 	$.getJSON("/brands", function(data) {
+		$("#brand").remove();
+		$("#category").remove();
 	    for(var i in data) { 
 	        $("#brand").append($("<option></option>").text(data[i].name));
 	        $("#category").append($("<option></option>").text(data[i].category));
--- src/main/resources/static/js/store-view.js
+++ src/main/resources/static/js/store-view.js
@@ -1,30 +1,65 @@
-$(document).ready(function() {
-	$("div#addproduct").hide();
-		$("#addprodcttostore").click(function() {
-			$("div#table").hide(500);
-			$("div#addproduct").show(900);
-		});
-		$("#addproductcancel").click(function() {
-			$("div#addproduct").hide(500);
-			$("div#table").show(900);
-		});
-		$('#list').change(function() {
-			$.getJSON("/products",function(data){
-				$("#products tbody").remove();
-				$("#products").append('<tbody></tbody>');
-				for(var i in data)
-				{
-					var tblRow = "<tr>" 
-				+ "<td>" + data[i].id + "</td>" 
-				+ "<td>" + data[i].name + "</td>" 
-	           	+ "<td>" + data[i].price + "</td>"
-	           	+ "<td>" + data[i].type + "</td>" 
-	           	+ "<td>" + data[i].brand + "</td>" 
-	           	+ "<td>" + data[i].category + "</td>" 
-	           	+ "<td>" + data[i].quantity + "</td>" 
-	           	+ "</tr>"
-	           	$("#products tbody").append(tblRow);
+$(document).ready(
+		function() {
+			$("div#addproduct").hide();
+			$("#addprodcttostore").click(function() {
+				$("div#table").hide(500);
+				$("div#addproduct").show(900);
+			});
+			$("#addproductcancel").click(function() {
+				$("div#addproduct").hide(500);
+				$("div#table").show(900);
+			});
+
+			$.getJSON("/brandstore", {
+				pname : $('#ProductName').text()
+			}, function(data) {
+				for ( var i in data) {
+					$("#Brand").append($("<option></option>").text(data[i].name));
+					$("#Brand").append($("<option></option>").text(data[i].category));
 				}
+			});
+
+			$.getJSON("/productstore", {
+				bname : $('#Brand').text()
+			}, function(data) {
+				for ( var i in data) {
+					$("#ProductName").append($("<option></option>").text(data[i].name));
+				}
+			});
+
+			$('#ProductName').change(
+					function() {
+						$.getJSON("/brandstore", {pname : $('#ProductName').text()}, function(data) {
+							$("#Brand").append($("<option></option>").text(data[i].name));
+							$("#Brand").append($("<option></option>").text(data[i].category));
+						});
+					});
+
+			$('#Brand').change(
+					function() {
+						$.getJSON("/productstore", {bname : $('#Brand').text()}, function(data) {
+							for ( var i in data) {
+								$("#ProductName").append($("<option></option>").text(data[i].name));
+							}
+						});
+					});
+
+			$('#list').change(
+					function() {
+						$.getJSON("/products", function(data) {
+							$("#products tbody").remove();
+							$("#products").append('<tbody></tbody>');
+							for ( var i in data) {
+								var tblRow = "<tr>" + "<td>" + data[i].id
+										+ "</td>" + "<td>" + data[i].name
+										+ "</td>" + "<td>" + data[i].price
+										+ "</td>" + "<td>" + data[i].type
+										+ "</td>" + "<td>" + data[i].brand
+										+ "</td>" + "<td>" + data[i].category
+										+ "</td>" + "<td>" + data[i].quantity
+										+ "</td>" + "</tr>"
+								$("#products tbody").append(tblRow);
+							}
+						});
+					});
 		});
-		});	
-});
