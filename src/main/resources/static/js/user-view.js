df50827d1881692c0b681bb7eeb68c0683fa80fb
MOhamedHDrmi <hdrmim7@gmail.com>
2018-04-10 00:58:04 +0200

    normal user js

---
+++ src/main/resources/static/js/user-view.js
@@ -0,0 +1,12 @@
+-$(document).ready(function() {
+	$('#topbar').hide();
+	$('#userwindow').hide();
+	$('#search').click(function(){
+		$('#userwindow').hide(500);
+		$('#topbar').toggle(500);
+	});
+	$("#buyproduct").click(function() {
+		$("div#userwindow").hide();
+		$("div#userwindow").empty();
+	});
+});
