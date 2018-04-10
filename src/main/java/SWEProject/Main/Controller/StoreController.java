0bf850d354e3d2c0c6803755e01a3a5566da38ce
nouradel975 <34551965+nouradel975@users.noreply.github.com>
2018-04-10 23:39:21 +0200

    fix conflict

--- src/main/java/SWEProject/Main/Controller/StoreController.java
+++ src/main/java/SWEProject/Main/Controller/StoreController.java
@@ -1,3 +1,5 @@
+
+
 package SWEProject.Main.Controller;
 import SWEProject.Main.Controller.Entities.*;
 import SWEProject.Main.Controller.Repository.*;
@@ -9,74 +11,96 @@ import java.util.ArrayList;
 import java.util.List;
 @Controller
 public class StoreController {
-	@Autowired
-	public StoreRepository storeRepo;
-	@Autowired
-	public StoreProductRepository storeProductRepo;
-	@Autowired
-	public StatisticsRepository statRepo;
-	@Autowired
-	public SystemProductRepository  sysProRepo;
+    @Autowired
+    public StoreRepository storeRepo;
+    @Autowired
+    public StoreProductRepository storeProductRepo;
+    @Autowired
+    public StatisticsRepository statRepo;
+    @Autowired
+    public SystemProductRepository sysProRepo;
 
-	@RequestMapping("/ShowOwnerStores")
-	@ResponseBody
-	public  List<Store> showAllStores(){
-		StoreOwner user = (StoreOwner) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
-		return storeRepo.findByStoreOwnerAndStatus(user, "accepted");
-	}
-	@RequestMapping("/add-product-store/{storeName}")
-	@ResponseBody
-	public void addProduct(@RequestBody() StoreProduct p,@PathVariable("storeName")String sname) {
 
-		boolean x=true;
-		Store store = storeRepo.findOneByStoreName(sname);
-		for(int i=0;i<store.getProducts().size();i++){
-			if(store.getProducts().get(i).getName().equals(p.getName())){
-				x=false;
-				break;
-			}
-		}
-		if(x) {
+    @RequestMapping("/ShowOwnerStores")
+    @ResponseBody
+    public List<Store> showAllStores() {
+        StoreOwner user = (StoreOwner) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        return storeRepo.findByStoreOwnerAndStatus(user, "accepted");
+    }
 
-			p.setStore(store);
-			/*SystemProduct product = sysProductrepo.findOneByName(p.getName());
-			StoreProduct storeProduct = new StoreProduct(p.getQuantity(), p.getPrice(), s);
-			storeProduct.setName(product.getName());
-			storeProduct.setBrand(product.getBrand());
-			storeProduct.setType(product.getType());
-			s.addProduct(storeProduct);
-			storeProductRepo.save(storeProduct);*/
-			AddProductCommand add = new AddProductCommand(p);
-			add.execute(this);
-		}
-	}
+    @GetMapping("/add-product-to-store")
+    public String addproduct() {
+        return "add-product-to-store";
+    }
 
-	@RequestMapping("/openStore")
-	@ResponseBody
-	public  List<StoreProduct> openStore(@RequestBody String sname){
-		List<StoreProduct> sProducts = new ArrayList<StoreProduct>();
-		Store s=storeRepo.findOneByStoreName(sname);
-		for (int i=0;i<s.getProducts().size();i++) {
-			sProducts.add(s.getProducts().get(i));
-		}
-		statRepo.updateNumUserView(sname);
-		return  sProducts;
-	}
-	@PostMapping("/add-store")
-	@ResponseBody
-	public void newStore(@RequestBody Store store,@RequestParam("type") String type) {
-		if (!storeRepo.exists(store.getStoreName())) {
-			StoreOwner storeOwner = new StoreOwner(
-					(User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
-			store = Creator.getInstance().createStore(type, store, storeOwner);
-			storeOwner.addStore(store);
-			storeRepo.save(store);
-		}
-	}
-	@GetMapping("/store-view")
-	public List<Store> StoreOwnerStores() {
+    @RequestMapping("/add-product-store/{storeName}")
+    @ResponseBody
+    public void addProduct(@RequestBody() StoreProduct product, @PathVariable("storeName") String sname) {
 
-		List<Store> stores = storeRepo.findByStatus("accepted");
-		return stores;
-	}
+        boolean exist = false;
+        Store store = storeRepo.findOneByStoreName(sname);
+
+        for (int i = 0; i < store.getProducts().size(); i++) {
+
+            if (store.getProducts().get(i).getName().equals(product.getName())) {
+
+                exist = true;
+                break;
+            }
+        }
+
+        if (!exist) {
+
+            product.setStore(store);
+            AddProductCommand add = new AddProductCommand(product/*,"added product"*/);
+            add.execute(this);
+        }
+    }
+
+    @RequestMapping("/delete-product-store")
+    @ResponseBody
+    public void deleteProduct(@RequestBody() StoreProduct product) {
+
+        DeleteProductCommand deleteProduct = new DeleteProductCommand(product/*,"deleted product"*/);
+        deleteProduct.execute(this);
+    }
+
+    @RequestMapping("/openStore")
+    @ResponseBody
+    public List<StoreProduct> openStore(@RequestBody String sname) {
+        List<StoreProduct> sProducts = new ArrayList<StoreProduct>();
+        Store s = storeRepo.findOneByStoreName(sname);
+        for (int i = 0; i < s.getProducts().size(); i++) {
+            sProducts.add(s.getProducts().get(i));
+        }
+        statRepo.updateNumUserView(sname);
+        return sProducts;
+    }
+
+    @PostMapping("/add-store")
+    @ResponseBody
+    public void newStore(@RequestBody Store store, @RequestParam("type") String type) {
+        if (!storeRepo.exists(store.getStoreName())) {
+            StoreOwner storeOwner = new StoreOwner(
+                    (User) SecurityContextHolder.getContext().getAuthentication().getPrincipal());
+            store = Creator.getInstance().createStore(type, store, storeOwner);
+            storeOwner.addStore(store);
+            storeRepo.save(store);
+        }
+    }
+
+    @GetMapping("/store-view")
+    public List<Store> StoreOwnerStores() {
+
+        StoreOwner user = (StoreOwner) SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        List<Store> stores = storeRepo.findByStatus("accepted");
+        return stores;
+    }
+
+    /*@RequestMapping("/store-commands")
+    @ResponseBody
+    public List<Command> getStoreCommands() {
+
+
+    }*/
 }
